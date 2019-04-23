b7c58cfc6f0b06e8ee8f6136e743440961ba5121
Nicholas <nicholaslwallace@gmail.com>
2019-04-23 13:00:45 -0600

    Vimba class
    Created Vimba class to handle opening and closing of camera. Will allow asynchronous opening behavior

--- FINIS-Console/FINIS-Console.cpp
+++ FINIS-Console/FINIS-Console.cpp
@@ -1,5 +1,4 @@
-// ConsoleTesting.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// FINIS-Console.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "pch.h"
 #include <iostream>
@@ -10,41 +9,16 @@
 #include <windows.h>
 // PIXCI header
 #include "XCLIB/xcliball.h"
-// Vimba header
-#include "VimbaCPP/Include/VimbaCPP.h"
 
+// Vimba class
+#include "Vimba.h"
 
 //header files we have written
 #include "IMU.h"
 
 int main() {
-
-
-	AVT::VmbAPI::VimbaSystem &sys = AVT::VmbAPI::VimbaSystem::GetInstance();
-	AVT::VmbAPI::CameraPtrVector cameras;
-	AVT::VmbAPI::CameraPtr camera;
-	AVT::VmbAPI::FeaturePtr feature;
-
-	sys.Shutdown();
-
-	sys.Startup();
-	sys.GetCameras(cameras);
-	std::cout << cameras.size() << "\n";
-	camera = cameras[0];
-
-	sys.OpenCameraByID("DEV_64AA2C448F1F2349", VmbAccessModeFull, camera);
-	camera->GetFeatureByName("ExposureTime", feature);
-	feature->SetValue(30000.0);
-	camera->GetFeatureByName("AcquisitionFrameRate", feature);
-	feature->SetValue(30.0);
-	camera->GetFeatureByName("SensorGain", feature);
-	feature->SetValue("Gain1");
-	camera->GetFeatureByName("SensorTemperatureSetpointValue", feature);
-	feature->SetValue(20);
-	camera->GetFeatureByName("SensorTemperatureSetpointSelector", feature);
-	feature->SetValue(1);
-	camera->GetFeatureByName("AcquisitionStart", feature);
-	feature->RunCommand();
+	Vimba vimba = Vimba();
+	vimba.startCamera();
 
 	/*
 	//Initial Set up for the IMU
@@ -115,7 +89,4 @@ int main() {
 	pxd_PIXCIclose();
 	std::cout << "Hello World!\n";
 
-	camera->Close();
-	sys.Shutdown();
-	std::cout << "Shutdown\n";
 }
---
+++ FINIS-Console/Vimba.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Vimba.h"
+
+/**
+ * Constructor
+ * Initialize startup behavior, but do not open the camera
+ */
+Vimba::Vimba() {
+	sys = AVT::VmbAPI::VimbaSystem::GetInstance();
+}
+
+/**
+ * Destructor
+ * Close camera before shutdown
+ */
+Vimba::~Vimba() {
+	std::cout << "Closing camera...";
+	camera->Close();
+	sys.Shutdown();
+	std::cout << "done\n";
+}
+
+/**
+ * Start the camera
+ * This function can be called asyncronously so that other startup behavior can occur
+ * Returns TRUE upon successful completion
+ */
+bool Vimba::startCamera() {
+	// Shutdown system in case it was not properly closed
+	sys.Shutdown();
+
+	// Start system and get first camera (this instrament only has one camera attached)
+	sys.Startup();
+	sys.GetCameras(cameras);
+	std::cout << cameras.size() << "\n";
+	camera = cameras[0];
+
+	// Set features for the camera
+	sys.OpenCameraByID("DEV_64AA2C448F1F2349", VmbAccessModeFull, camera);
+	camera->GetFeatureByName("ExposureTime", feature);
+	feature->SetValue(30000.0);
+	camera->GetFeatureByName("AcquisitionFrameRate", feature);
+	feature->SetValue(30.0);
+	camera->GetFeatureByName("SensorGain", feature);
+	feature->SetValue("Gain1");
+	camera->GetFeatureByName("SensorTemperatureSetpointValue", feature);
+	feature->SetValue(20);
+	camera->GetFeatureByName("SensorTemperatureSetpointSelector", feature);
+	feature->SetValue(1);
+	camera->GetFeatureByName("AcquisitionStart", feature);
+	feature->RunCommand();
+
+	return true;
+}
